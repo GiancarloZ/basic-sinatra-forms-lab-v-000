3b1f0116f5b06ccf66aec7c24e89f8547a02e677
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-23 20:15:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ class App < Sinatra::Base
   post "/team" do
     @name = params[:name]
     @coach = params[:coach]
+    @pg = params[:pg]
     @sg = params[:sg]
     @sf = params[:sf]
-    @sg = params[:pf]
-    @sf = params[:sf]
+    @pf = params[:pf]
     @center = params[:center]
     erb :team 
   end
